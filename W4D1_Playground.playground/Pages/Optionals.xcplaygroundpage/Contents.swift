07fab3e2338fe5f2a747febc5db71cafadb4e740
Colin Russell <colinrussell101@gmail.com>
2018-04-30 13:02:20 -0400

    finished optionals

--- W4D1_Playground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ W4D1_Playground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -19,14 +19,14 @@ var doubleNumberValue: Double? = nil
  - Experiment:
  Declare a non-optional variable of a `String` and set it to `nil`. What happens?
  */
-
+//var nonOptional: String = nil error
 
 /*:
  - Experiment:
  Declare an optional variable of a type `String` and set an initial `String` value to it. Then set it to `nil` on the next line. Does this work? Why or why not?
  */
-
-
+var optional: String = "value"
+//optional = nil does not work because you already assigned it a value making it not optional anymore
 /*:
  Let's consider multiplying two numbers together that are of different types. Since we have a `Double` and an `Int`, we need to convert the two numbers to the same type. For example, creating a new `Double` using an `Int`.
  */
@@ -47,13 +47,14 @@ ratio * convertIntegerValue // now this works!
  - Experiment:
  Declare a `String` containing digits and try converting it to a `Double` the same way shown in the above example. What do you notice about the variable type? Hint: Use 'Option' + Mouse Click on the variable to see the type
  */
-
-
+//var stringWithDigits = "no"
+var stringWithDigits = "3.2"
+var doubleFromString = Double(stringWithDigits)
 /*:
  - Experiment:
  With your newly converted `Double` from a `String`, try multiplying it with the 'ratio' variable. What happens?
  */
-
+//var result = doubleFromString*ratio not unwrapped error
 
 /*:
  Your newly converted `Double` value is a `Double?` which indicates we might have a double or we might have nothing.  Converting a `String` to a `Double` might fail because the `String` does not guarantee there will only be digits within it.
@@ -77,29 +78,29 @@ print("\(myOptionalDouble!)")
   - Experiment:
  Now you try! Try printing out your converted `Double?` with a force unwrap
 */
-
-
-
+print("\(doubleFromString!)") // no error
 /*:
  - Experiment:
  Go back and change your `String` to something that has no digits. What happens and why?
  */
-
-
+// fatal error because doubleFromString is set to nil since it doesn't have any digits
 
 /*:
  - Experiment:
  Declare an optional variable of a type `String` and set an initial `String` value to it. Try printing it.
  Now print it again, but this time unwrap the optional variable using the `'!'`. What's different about the two lines you printed?
  */
-
-
+var optionalString: String? = "initial string"
+print(optionalString)
+print(optionalString!)
+// it prints the word Optional with my string in brackets
 /*:
  - Experiment:
  Try setting an optional `String` variable to a non-optional `String` variable. What happens? What can you do to prevent the compiler from throwing an error?
  */
-
-
+var notOptionalString = "not optional"
+optionalString = notOptionalString
+print(optionalString)
 /*:
  The next way to deal with optionals is called `"Conditional unwrapping"` or sometimes casually called an `"if-let"`. It's **much** safer, and won't break your Playground, or any of your code.
  
@@ -107,6 +108,7 @@ print("\(myOptionalDouble!)")
 */
 
 let gravityConstant: Double? = 9.8
+//let gravityConstant: Double? = nil
 
 if let unwrapped = gravityConstant {
     // unwrapped exists in this block, and is number unwrapped.
@@ -121,19 +123,19 @@ if let unwrapped = gravityConstant {
  - Experiment:
  Try changing `gravityConstant` to nil. What happens?
  */
-
+// doesn't allow the gravityConstant to be unwrapped
 
 /*:
  - Experiment:
  Create an array with containing elements of any type of your choice. Try experimenting with the array methods `'first'` and `'last'` to find out what they do. You'll see that both return optional values. Print out the values of first and last by using conditional unwrapping.
  */
-
-
+var arrayOfLetters = ["c", "b", "e", "i", "o"]
+print("first: \(arrayOfLetters.first!) last: \(arrayOfLetters.last!)")
 /*:
  - Experiment:
  Using the same array, experiment with the array method `'indexOf'` and find out what it does. Print out the value using conditional unwrapping.
  */
-
+print("indexOf method: \(arrayOfLetters.index(of: "b")!)")
 
 /*:
  - Callout(Challenge):
